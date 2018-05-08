ddd2cc5b2793e2bd4186b9bccd20ea7e8dddc67d
Marhula <mino4560@gmail.com>
2018-05-08 12:57:17 +0200

    Pridanie .htaccess a vztvorenie headera a footera

---
+++ application/views/templates/header.php
@@ -0,0 +1,11 @@
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $title; ?></title>
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="<?php echo base_url();?>assets/css/bootstraptheme.min.css" rel="stylesheet">
+
+</head>
+<body>
